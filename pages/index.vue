e9565a0ff5d896c4878a01d3c8c2e6e868175601
Ionut Nechita <ionut_n2001@yahoo.com>
2025-05-01 22:05:51 +0300

    Simple Hello World in Nuxtjs
    Description:
     - Simple Hello World in Nuxtjs
    
    Change-Id: Ib6b6988d7e4c245bdfefad8b4106b7d7902d2070
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello-world-container">
+    <h1 class="title">Hello World!</h1>
+    <p class="subtitle">Welcome to your first Nuxt.js application.</p>
+    <button @click="incrementCounter" class="button">
+      Press me! ({{ counter }})
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello-world-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+  font-family: 'Arial', sans-serif;
+}
+
+.title {
+  font-size: 4rem;
+  color: #41b883;
+  margin-bottom: 1rem;
+}
+
+.subtitle {
+  font-size: 1.5rem;
+  color: #35495e;
+  margin-bottom: 2rem;
+}
+
+.button {
+  background-color: #41b883;
+  color: white;
+  border: none;
+  padding: 0.75rem 1.5rem;
+  font-size: 1rem;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.button:hover {
+  background-color: #3aa876;
+}
+</style>
